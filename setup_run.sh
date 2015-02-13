0afccfa915eea382b396f2eb9a64bfed886bdf8d
Jeremy Schaub <schaubjeremy@gmail.com>
2015-02-13 13:58:54 -0600

    Interim commit

--- setup_run.sh
+++ setup_run.sh
@@ -4,12 +4,12 @@ RUNTYPE=DATA
 [[ $# -gt 1 ]] && RUNTYPE=$1
 
 CWD=$(pwd)
-export RUNDIR=../rundir/$(date +"$RUNTYPE-%Y%m%d-%H%M")
-export RAWDIR=$RUNDIR/data/raw
-export FINALDIR=$RUNDIR/data/final
-export SCRIPTDIR=$RUNDIR/scripts
-export IMGDIR=$RUNDIR/img
-export HTMLDIR=$RUNDIR/html
+RUNDIR=../rundir/$(date +"$RUNTYPE-%Y%m%d-%H%M")
+RAWDIR=$RUNDIR/data/raw
+FINALDIR=$RUNDIR/data/final
+SCRIPTDIR=$RUNDIR/scripts
+IMGDIR=$RUNDIR/img
+HTMLDIR=$RUNDIR/html
 mkdir -p $RAWDIR
 mkdir -p $FINALDIR
 mkdir -p $SCRIPTDIR
@@ -17,4 +17,4 @@ mkdir -p $IMGDIR
 rm -f ../rundir/$RUNTYPE-latest
 ln -sf $RUNDIR ../rundir/$RUNTYPE-latest
 cp $0 $SCRIPTDIR   # copy this script to the script directory
-
+echo $RUNDIR
