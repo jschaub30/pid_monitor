5a19cec6a18b4ade3277c0bb6f6cf6081c6e4d3b
Jeremy Schaub <schaubjeremy@gmail.com>
2015-03-09 15:07:13 -0500

    Updated html/javascript with buttons

--- html/js/charts.js
+++ html/js/charts.js
@@ -1,52 +1,65 @@
-cpu_chart = function(id, csv_fn){
-  id = id.replace(/ /g, '_');  // Replace all spaces
-  // Add h3 title
-  $('<h3></h3>', {
-    text: id,
-  }).appendTo('body');
 
-  // Add chart div
-  div = $('<div></div>',{
-    class: "chart",
-    id: "id_" + id
-  }).appendTo('body');
-
-  //Create c3js chart
-  var chart = c3.generate({
-    bindto: "#id_" + id,
-    data: {
-      url: csv_fn,
-      x: 'x',
+//Create c3js chart
+var cpu_chart = c3.generate({
+  bindto: "#id_cpu",
+  data: {
+    url: cpu_csv_fn,
+    x: 'x',
+  },
+  type: 'line',
+  grid: {
+    x: {
+      show: true
     },
-    type: 'line',
-    grid: {
-      x: {
-        show: true
-      },
-      y: {
-        show: true
-      }
-    },
-    point: {
-      r: 5
-    },
-    axis: {
-      y: {
-        // min: 0,
-        // max: 100,
-        label: 'Usage [ % ]',
-      },
-      x: {
-        // min: 0,
-        // max: 60,
-        label: 'Elapsed time [ sec ]',
-      }
+    y: {
+      show: true
     }
-  });
-}
+  },
+  point: {
+    r: 5
+  },
+  axis: {
+    y: {
+      // min: 0,
+      // max: 100,
+      label: 'Usage [ % ]',
+    },
+    x: {
+      // min: 0,
+      // max: 60,
+      label: 'Elapsed time [ sec ]',
+    }
+  }
+});
 
-cpu_chart('CPU', 'cpu_pct.csv');
-
-cpu_chart('Memory', 'mem_pct.csv');
-
-
+var mem_chart = c3.generate({
+  bindto: "#id_mem",
+  data: {
+    url: mem_csv_fn,
+    x: 'x',
+  },
+  type: 'line',
+  grid: {
+    x: {
+      show: true
+    },
+    y: {
+      show: true
+    }
+  },
+  point: {
+    r: 5
+  },
+  axis: {
+    y: {
+      // min: 0,
+      // max: 100,
+      label: 'Usage [ % ]',
+    },
+    x: {
+      // min: 0,
+      // max: 60,
+      label: 'Elapsed time [ sec ]',
+    }
+  }
+});
