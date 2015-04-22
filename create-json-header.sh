dff837dd28348f1bf27b61146163f920b99984f0
Jeremy Schaub <schaubjeremy@gmail.com>
2015-04-22 16:59:54 -0500

    Added more varibles to json header.

--- create-json-header.sh
+++ create-json-header.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-# Creates JSON header based on WORKLOAD_NAME and RUNDIR
+# Creates JSON header based on these workspace variables
+# $WORKLOAD_NAME 
+# $RUNDIR
+# $X_LABEL
+# $DESCRIPTION
 
 
-[[ -z "$WORKLOAD_NAME" ]] && [[ -z "$RUNDIR" ]] && echo $0: WORKLOAD_NAME and/or RUNDIR not set && exit -1
+[[ -z "$RUNDIR" ]] && echo $0: !!!!!!!!!! Warning!  RUNDIR not set 
+[[ -z "$WORKLOAD_NAME" ]] && WORKLOAD_NAME="WORKLOAD"
+[[ -z "$X_LABEL" ]] && X_LABEL="X label"
+[[ -z "$DESCRIPTION" ]] && DESCRIPTION="DESCRIPTION"
 
 echo \{\"workload\":\"$WORKLOAD_NAME\", >> $RUNDIR/html/config.json
 echo \"date\":\"$(date)\", >> $RUNDIR/html/config.json
+echo \"hostname\":\"$(hostname -s)\", >> $RUNDIR/html/config.json
+echo \"description\":\"$DESCRIPTION\", >> $RUNDIR/html/config.json
+echo \"xlabel\":\"$X_LABEL\", >> $RUNDIR/html/config.json
 echo \"run_ids\":\[ >> $RUNDIR/html/config.json
 
