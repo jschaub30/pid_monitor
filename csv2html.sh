b6e3c4e4caa39b1c2a65c4a586d746599ccc4fd7
Jeremy Schaub <schaubjeremy@gmail.com>
2015-04-29 14:55:59 -0500

    Added header to html summary table

--- csv2html.sh
+++ csv2html.sh
@@ -4,7 +4,9 @@
 CSV_FN=$1
 
 echo "<table>"
-cat $CSV_FN | \
+head -n 1 $CSV_FN | \
+    sed -e 's/^/<tr><th>/' -e 's/,/<\/th><th>/g' -e 's/$/<\/th><\/tr>/'
+tail -n +2 $CSV_FN | \
     sed -e 's/^/<tr><td>/' -e 's/,/<\/td><td>/g' -e 's/$/<\/td><\/tr>/'
 echo "</table>"
 
