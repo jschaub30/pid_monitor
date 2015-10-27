502ca74bc15fe49e2b8068a95ebe3c43da20876c
Jeremy Schaub <schaubjeremy@gmail.com>
2015-10-27 09:15:16 -0500

    Turned on write permissions in /tmp/pid_monitor directory

--- start_dstat.sh
+++ start_dstat.sh
@@ -9,6 +9,7 @@ DELAY_SEC=$3
 [ "$DELAY_SEC" -lt "1" ] && echo Setting DELAY_SEC to 1 instead of $3; DELAY_SEC=1
 
 DSTAT_CMD="mkdir -p /tmp/pid_monitor/; \
+           chmod 777 /tmp/pid_monitor; \
            rm -f $DSTAT_FN; \
            sleep 0.1; \
            dstat --time -v --net --output $DSTAT_FN $DELAY_SEC"
