14519ff0c557f3995431b70ed9631c2c4d45c804
Jeremy Schaub <schaubjeremy@gmail.com>
2015-12-16 15:00:51 +0000

    Increased thresholds on watch_dstat; Fixed delay bug

--- start_dstat.sh
+++ start_dstat.sh
@@ -5,14 +5,13 @@
 HOST=$1
 DSTAT_FN=/tmp/pid_monitor/$(basename $2)
 DELAY_SEC=$3
-
-[ "$DELAY_SEC" -lt "1" ] && echo Setting DELAY_SEC to 1 instead of $3; DELAY_SEC=1
+[ "$DELAY_SEC" -lt "1" ] && echo Setting DELAY_SEC to 1 instead of $3 && DELAY_SEC=1
 
 DSTAT_CMD="mkdir -p /tmp/pid_monitor/; \
            chmod 777 /tmp/pid_monitor; \
            rm -f $DSTAT_FN; \
            sleep 0.1; \
            dstat --time -v --net --output $DSTAT_FN $DELAY_SEC"
-
+echo $DSTAT_CMD
 $(ssh $HOST $DSTAT_CMD) 2>/dev/null &
 
--- watch_dstat.sh
+++ watch_dstat.sh
@@ -3,15 +3,15 @@
 #"procs",,,"memory usage",,,,"paging",,"dsk/total",,"system",,"total cpu usage",,,,,,"system","net/total",
 VARS=( run  blk  new  used  buff  cach  free  in  out  read  writ  int  csw  usr  sys  idl  wai  hiq  siq  time  recv  send )
 
-THRESH[10]=100 #read
-THRESH[11]=100 #write
+THRESH[10]=800 #read
+THRESH[11]=800 #write
 THRESH[12]=100 #int
 THRESH[13]=100 #csw
 THRESH[14]=2   #usr
 THRESH[15]=2   #sys
 THRESH[17]=2   #wai
-THRESH[21]=100 #recv
-THRESH[22]=100 #send
+THRESH[21]=800 #recv
+THRESH[22]=800 #send
 
 watch_dstat () {
   echo ##################################################
@@ -47,7 +47,7 @@ RC=1
 while [ $RC -ne 0 ]
 do
   watch_dstat
-  sleep 5
+  sleep 10
 done
 
 echo System appears to be idle!
