a9e53bf7e6a1281482d029a8473e26b3bc796b8a
Jeremy Schaub <schaubjeremy@gmail.com>
2016-01-15 13:58:50 -0600

    Changed memory counters

--- memory_bw.R
+++ memory_bw.R
@@ -16,7 +16,8 @@ tot_ld_l_L2 <- d$PM_DATA_FROM_L2 / (1024*1024*1024) #Total Loads from local L2
 d1$L2 <- L1_cache_line_size * tot_ld_l_L2
 tot_ld_l_L3 <- d$PM_DATA_FROM_L3 / (1024*1024*1024) #Total Loads from local L3
 d1$L3 <- L2_cache_line_size * tot_ld_l_L3
-tot_ld_mem = (d$PM_DATA_FROM_RMEM + d$PM_DATA_FROM_LMEM) / (1024*1024*1024)
+#tot_ld_mem = (d$PM_DATA_FROM_RMEM + d$PM_DATA_FROM_LMEM) / (1024*1024*1024)
+tot_ld_mem = d$PM_DATA_ALL_FROM_MEMORY / (1024*1024*1024)
 d1$MEM <- L3_cache_line_size * tot_ld_mem
 write.csv(d1, row.names=FALSE, quote=FALSE, stdout())
 
