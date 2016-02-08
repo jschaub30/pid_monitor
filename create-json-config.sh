d91109b4dc1df420da3e720f5829b5cce5befe99
Jeremy Schaub <schaubjeremy@gmail.com>
2016-02-08 12:15:47 -0600

    Added monitor list to config.json output

--- create-json-config.sh
+++ create-json-config.sh
@@ -18,6 +18,12 @@ echo \"date\":\"$(date)\", >> $CONFIG_FN
 echo \"hostname\":\"$(hostname -s)\", >> $CONFIG_FN
 echo \"data_dir\":\"$DATA_DIR\", >> $CONFIG_FN
 echo \"description\":\"$DESCRIPTION\", >> $CONFIG_FN
+echo \"monitors\":[\"dstat\" >> $CONFIG_FN
+[ "$GPU_FLAG" == "1" ] && echo ,\"gpu\" >> $CONFIG_FN
+[ "$OCOUNT_FLAG" == "1" ] && echo ,\"ocount\" >> $CONFIG_FN
+[ "$PERF_FLAG" == "1" ] && echo ,\"perf\" >> $CONFIG_FN
+[ "$AMESTER_FLAG" == "1" ] && echo ,\"amester\" >> $CONFIG_FN
+echo ], >> $CONFIG_FN
 echo \"xlabel\":\"$X_LABEL\", >> $CONFIG_FN
 echo \"stdout_ext\":\"$STDOUT_EXT\", >> $CONFIG_FN
 echo \"stderr_ext\":\"$STDERR_EXT\", >> $CONFIG_FN
