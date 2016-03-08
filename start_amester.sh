d462008008d5fc22708662fc02375db4f2031e29
Jeremy Schaub <schaubjeremy@gmail.com>
2016-03-08 17:07:48 -0600

    Fixed amester bugs

--- start_amester.sh
+++ start_amester.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
 # This script starts the amester program
-# AM_HOST is the host where the amester program is installed
+# AMESTER_IP is the IP of the host where amester is installed
 # BMC_IP is the address of the BMC/service processor
 # export BMC_USER and BMC_PASS if different than the default
 
-[ "$#" -ne "5" ] && echo "Usage: $0 HOSTNAME FN DELAY_MS AMESTER_IP BMC_IP" && exit 64
+[ "$#" -ne "4" ] && echo "Usage: $0 AMESTER_IP FN DELAY_SEC BMC_IP" && exit 64
 
-HOST=$1
+AMESTER_IP=$1
 FN=/tmp/pid_monitor/$(basename $2)
 DELAY_MS=$(($3*1000))
-AM_HOST=$4
-BMC_IP=$5
+BMC_IP=$4
 [ -z "$BMC_USER" ] && BMC_USER=ADMIN
 [ -z "$BMC_PASS" ] && BMC_PASS=admin
 
 [ "$DELAY_MS" -lt "1000" ] && echo Setting DELAY_MS to 1000 instead of $DELAY_MS && DELAY_MS=1000
 
-echo Checking to see if amester is running on $HOST
-CMD="ps -efa | grep amester | grep -v grep | grep -v $0 | grep -v vim | wc -l"
-RC=$(ssh $HOST $CMD)
+echo Checking to see if amester is running on $AMESTER_IP
+CMD='ps -efa | grep amester | grep -v grep | grep -v vim | wc -l'
+RC=$(ssh $AMESTER_IP $CMD)
 if [ $RC -ne 0 ]
 then
-  echo **************************************************
-  echo WARNING: amester appears to be running on $HOST.
+  echo "######################################################"
+  echo WARNING: amester appears to be running on $AMESTER_IP.
   echo Continuing, since more than 1 copy can run...
-  echo **************************************************
+  echo "######################################################"
 fi
-echo Starting amester monitoring on $HOST
+echo Starting amester monitoring on $AMESTER_IP
 
 AM_CMD="amester --nogui /home/ubuntu/amester/watchsensors.tcl $BMC_IP $BMC_USER $BMC_PASS $FN $DELAY_MS"
 CMD="mkdir -p /tmp/pid_monitor/; \
@@ -34,6 +33,6 @@ CMD="mkdir -p /tmp/pid_monitor/; \
            rm -f $FN; \
            bash -c \"$AM_CMD &\""
 #echo $CMD
-$(ssh ubuntu@$AM_HOST $CMD) 2>/dev/null &
+$(ssh $AMESTER_IP $CMD) 2>/dev/null &
+echo Please wait for amester to start collecting data
 sleep 15  # Give amester a chance to establish communication and start up
-
