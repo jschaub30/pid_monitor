fb3a97e47bf4208ce52ceed6fdfb18b2057f5a6e
Jeremy Schaub <schaubjeremy@gmail.com>
2016-04-25 11:58:32 -0500

    Finished cpu detail parser

--- parse_dstat_cpu.py
+++ parse_dstat_cpu.py
@@ -18,25 +18,36 @@ def main(dstat_fn):
     blob = "system" + blob.split('"system"')[1].strip()
     lines = blob.split('\n')
     num_cpu = (len(lines[0].split(',')) - 1)/6 # subtract 1 for time vector
+    val_array = [[] for i in range(num_cpu)]
     START = True
     PRINT = False
     HEADER = True
     fields = ['TIME_SEC']
     lines = lines[2:]  # skip 2 header rows
-    js_string = '['
+    td_array = []
     for line in lines:
         vals = line.split(',')
         t = datetime.strptime(vals[0], '%d-%m %H:%M:%S')
         if START:
             t0 = t
             START = False
-        td = (t - t0).total_seconds()
+        td_array.append((t - t0).total_seconds())
         for cpu in range(num_cpu):
             cpu_usr = float(vals[cpu*6 + 1])
             cpu_sys = float(vals[cpu*6 + 2])
             val = cpu_usr + cpu_sys
-            js_string += '[%d,%d,%0.1f],' % (int(td), cpu, val)
-    sys.stdout.write(js_string[0:-1] + ']')
+            val_array[cpu].append(val)
+            #js_string += '[%d,%d,%0.1f],' % (int(td), cpu, val)
+    js_string = '{\n  "labels": [' + ','.join(['"%.0f' % i + '"' for i in td_array]) + '],\n'
+    js_string += '  "datasets": [\n'
+    for cpu in range(num_cpu):
+        js_string += '  {\n    "label": "%d",\n' % cpu
+        js_string += '    "data": [' + ','.join([str(i) for i in val_array[cpu]]) + ']\n  }'
+        if cpu == (num_cpu - 1):
+            js_string += ']\n}'
+        else:
+            js_string += ' ,'
+    sys.stdout.write(js_string)
 
 if __name__ == '__main__':
     main(sys.argv[1])
