9b974dd3467de633fc590e38c273049ed36813b6
Jeremy Schaub <schaubjeremy@gmail.com>
2016-05-10 15:50:22 -0500

    Analyze all nvprof

--- tidy_nvprof.sh
+++ tidy_nvprof.sh
@@ -13,8 +13,16 @@ cp $FN $NEW_FN
 [ $? -ne 0 ] && echo ERROR: nvprof not found && exit 1
 
 # Step 2: Convert to CSV
-CSV_FN=$(echo $NEW_FN | perl -pe "s/nvprof/gpu_bandwidth.csv/")
-nvprof --csv --print-gpu-trace --import-profile $NEW_FN 2> $CSV_FN
+I=0
+for FN in ${INPUT_DIR}/*nvprof
+do
+    CSV_FN=$(echo $FN | perl -pe "s/nvprof/gpu_bandwidth.csv/")
+    /usr/local/cuda-7.5/bin/nvprof --csv --print-gpu-trace --import-profile $FN 2> $CSV_FN &
+    I=$((I+1))
+    [ $((I % 4)) -eq 0 ] && wait  # 4 at a time
+done
+
+wait
 
 # Step 2: Pull out HtoD, DtoH and DtoD bandwidth data
 I=0
@@ -22,6 +30,6 @@ for FN in ${INPUT_DIR}/*gpu_bandwidth.csv
 do
     ./parse_gpu_bandwidth.py $FN &
     I=$((I+1))
-    [ $((I % 4)) -eq 0 ] && wait
+    [ $((I % 4)) -eq 0 ] && wait  # 4 at a time
 done
 wait
